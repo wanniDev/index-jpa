d354b25a348bf230fe7e1b93efa2bb1572c7ef22
HyungJoongChoi <hyungjoong.choi@opusm.io>
2023-02-16 23:19:19 +0900

    init project

---
+++ src/main/kotlin/com/example/jpaindex/JpaindexApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jpaindex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaindexApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaindexApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jpaindex/JpaindexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jpaindex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaindexApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
